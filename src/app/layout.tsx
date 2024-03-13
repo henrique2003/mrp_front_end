bfc99d80864ea21c0e7e87fe3d30a9fc33ab702b
henrique2003 <henrique.de.melo.cristioglu@gmail.com>
2024-03-12 21:18:03 -0300

    chore: add shadcnui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
 import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
+import { Poppins } from 'next/font/google'
 import './globals.css'
+import { cn } from '../lib/shadcnui/utils'
 
-const inter = Inter({ subsets: ['latin'] })
+const poppins = Poppins({ subsets: ['latin'], weight: '400' })
 
 export const metadata: Metadata = {
   title: 'Create Next App',
@@ -12,7 +13,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>): JSX.Element {
   return (
     <html lang="pt-br">
-      <body className={inter.className}>{children}</body>
+      <body className={cn(poppins.className)}>{children}</body>
     </html>
   )
 }
