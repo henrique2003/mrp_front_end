ec7d09c070976816042e12c8ad42e8b0212685c0
Henrique De Melo Cristioglu <henrique.cristioglu@threeo.com.br>
2024-03-15 09:05:37 -0300

    feat: add sidebar

---
+++ src/components/core/sidebar/components/sidebar-link-item.tsx
@@ -0,0 +1,24 @@
+import Link from 'next/link'
+import { twMerge } from 'tailwind-merge'
+
+type ISidebarLinkItem = {
+  icon: JSX.Element
+  description: string
+  href: string
+  focus?: boolean
+}
+
+export const SidebarLinkItem: React.FC<ISidebarLinkItem> = ({ icon, description, href, focus }) => {
+  const blurClass = 'text-gray-500 dark:text-gray-400'
+  const focusClass = 'bg-gray-100 text-gray-900  dark:bg-gray-800 dark:text-gray-50'
+
+  return (
+    <Link
+      className={twMerge(
+        'flex items-center gap-3 rounded-lg px-3 py-2 transition-all hover:text-gray-900 dark:hover:text-gray-50',
+        focus != null && focus ? focusClass : blurClass
+      )}
+      href={href}
+    > {icon} {description} </Link>
+  )
+}
--- src/components/modules/dashboard/index.tsx
+++ src/components/modules/dashboard/index.tsx
@@ -20,52 +20,14 @@ import {
   TableCell,
   TableBody,
   Table,
-  Separator
+  Separator,
+  SideBar
 } from '@/components/core'
 
 export function Dashboard(): JSX.Element {
   return (
     <div className="grid min-h-screen w-full overflow-hidden lg:grid-cols-[280px_1fr]">
-      <div className="hidden border-r bg-gray-100/40 lg:block dark:bg-gray-800/40">
-        <div className="flex h-full max-h-screen flex-col gap-2">
-          <div className="flex h-[60px] items-center border-b px-6">
-            <Link className="flex items-center gap-2 font-semibold" href="#">
-              <Package2Icon className="h-6 w-6" />
-              <span className="">Acme Inc</span>
-            </Link>
-            <Button className="ml-auto h-8 w-8" size="icon" variant="outline">
-              <BellIcon className="h-4 w-4" />
-              <span className="sr-only">Toggle notifications</span>
-            </Button>
-          </div>
-          <div className="flex-1 overflow-auto py-2">
-            <nav className="grid items-start px-4 text-sm font-medium">
-              <Link
-                className="flex items-center gap-3 rounded-lg bg-gray-100 px-3 py-2 text-gray-900  transition-all hover:text-gray-900 dark:bg-gray-800 dark:text-gray-50 dark:hover:text-gray-50"
-                href="#"
-              >
-                <ArrowDownIcon className="h-4 w-4" />
-                Incoming Payments
-                <Badge className="ml-auto flex h-6 w-6 shrink-0 items-center justify-center rounded-full">5</Badge>
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                <ArrowUpIcon className="h-4 w-4" />
-                Outgoing Payments
-              </Link>
-              <Link
-                className="flex items-center gap-3 rounded-lg px-3 py-2 text-gray-500 transition-all hover:text-gray-900 dark:text-gray-400 dark:hover:text-gray-50"
-                href="#"
-              >
-                <CreditCardIcon className="h-4 w-4" />
-                Payment History
-              </Link>
-            </nav>
-          </div>
-        </div>
-      </div>
+      <SideBar />
       <div className="flex flex-col">
         <header className="flex h-14 lg:h-[60px] items-center gap-4 border-b bg-gray-100/40 px-6 dark:bg-gray-800/40">
           <Link className="lg:hidden" href="#">
@@ -255,86 +217,6 @@ export function Dashboard(): JSX.Element {
         </main>
       </div>
     </div>
-  )
-}
-
-function ArrowDownIcon(props: any): JSX.Element {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M12 5v14" />
-      <path d="m19 12-7 7-7-7" />
-    </svg>
-  )
-}
-
-function ArrowUpIcon(props: any): JSX.Element {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="m5 12 7-7 7 7" />
-      <path d="M12 19V5" />
-    </svg>
-  )
-}
-
-function BellIcon(props: any): JSX.Element {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <path d="M6 8a6 6 0 0 1 12 0c0 7 3 9 3 9H3s3-2 3-9" />
-      <path d="M10.3 21a1.94 1.94 0 0 0 3.4 0" />
-    </svg>
-  )
-}
-
-function CreditCardIcon(props: any): JSX.Element {
-  return (
-    <svg
-      {...props}
-      xmlns="http://www.w3.org/2000/svg"
-      width="24"
-      height="24"
-      viewBox="0 0 24 24"
-      fill="none"
-      stroke="currentColor"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-    >
-      <rect width="20" height="14" x="2" y="5" rx="2" />
-      <line x1="2" x2="22" y1="10" y2="10" />
-    </svg>
   )
 }
 
